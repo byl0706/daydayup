4b2bdc57f3a28263fb8b93821d7567d1134bd0cf
baiyunlong <baiyunlong@css.com.cn>
2020-07-16 09:58:00 +0800

    提交

---
+++ src/arrays/Generated.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import latenttype.adapter.Generator;
+
+import java.lang.reflect.Array;
+
+/**
+ * @author baiyunlong
+ * @title Generated
+ * @description TODO
+ * @date 2020/7/15 14:34
+ */
+public class Generated {
+//    public static <T> T[] array(T[] a, Generator<T> gen) {
+//        return new CollectionData<T>(gen, a.length).toArray(a);
+//    }
+//
+//    @SuppressWarnings("unchecked")
+//    public static <T> T[] array(Class<T> type, Generator<T> gen, int size) {
+//        T[] a = (T[]) Array.newInstance(type, size);
+//        return new CollectionData<T>(gen, size).toArray(a);
+//    }
+}
---
+++ src/arrays/compare/CompType.java
@@ -0,0 +1,58 @@
+package arrays.compare;
+
+import arrays.Generated;
+import latenttype.adapter.Generator;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author baiyunlong
+ * @title CompType
+ * @description TODO
+ * @date 2020/7/15 14:18
+ */
+public class CompType implements Comparable<CompType> {
+
+    int i;
+    int j;
+    private static int count = 1;
+
+    public CompType(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    @Override
+    public int compareTo(CompType o) {
+        return i < o.i ? -1 : (i == o.i ? 0 : 1);
+    }
+
+    @Override
+    public String toString() {
+        String result = "[i = "+ i +", j = " + j + "]";
+        if (count++ % 3 == 0) {
+            result += "\n";
+        }
+        return result;
+    }
+
+    private static Random r = new Random(47);
+    public static Generator<CompType> generator() {
+        return new Generator<CompType>() {
+            @Override
+            public CompType next() {
+                return new CompType(r.nextInt(100), r.nextInt(100));
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+//        CompType[] a = Generated.array(new CompType[12], generator());
+        System.out.println("before sort:");
+//        System.out.println(Arrays.toString(a));
+//        Arrays.sort(a);
+        System.out.println("after sort:");
+//        System.out.println(Arrays.toString(a));
+    }
+}
